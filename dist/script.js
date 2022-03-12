077e21fddf41ba994a34981d181dbde0788bf825
Nate <BrightNate1022@gmail.com>
2022-03-12 07:34:46 -0500

    hey

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,36 @@
+
+var names = [];
+var obj = {};
+var test = "POP"
+
 var nodes = new vis.DataSet([
-  {label: "Pop"},
-  {label: "Alternative"},
-  {label: "Rock"},
-  {label: "Jazz"},
-  {label: "Hits"},
-  {label: "Dance"},
-  {label: "Metal"},
-  {label: "Experimental"},
-  {label: "Rap"},
-  {label: "Electronic"},
+
 ]);
+console.log("hello");
+
+  $.ajax({url: "/myjsonfile.json",  success: function(Name){
+  
+   obj = Name;
+   for (let i=0;i<obj.length;i++)
+    {
+    var namest= obj[i]
+    names.push(namest.Name)
+     
+    console.log(namest.Name)
+    nodes.add({label:names[i]})
+  } 
+  
+  console.log(names)  
+  }}); 
+
+  console.log("hello");
+  console.log(obj)
+  
+    
+  obj[0] = 
+  console.log("hello");
+console.log(nodes)
+
 var edges = new vis.DataSet();
 
 var container = document.getElementById('bubbles');
@@ -47,3 +68,5 @@ container.on("mouse-wheel", function(event) {
   // prevents zooming with the mouse-wheel event
   event.stopPropagation();
 });
+
+console.log("hello");
